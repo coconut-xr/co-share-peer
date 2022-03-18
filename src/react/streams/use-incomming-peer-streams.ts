21487e49c6dc08024af89dd517e6fb31a32b0ba9
Bela Bohlender <bbohlender@coconut-capital.com>
2022-03-18 13:21:53 +0100

    fix startup incomming streams

--- src/react/streams/use-incomming-peer-streams.ts
+++ src/react/streams/use-incomming-peer-streams.ts
@@ -1,24 +1,20 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { StreamingInstance } from "../../observe-streams"
 
 export function useIncommingPeerStreamsChange(
     peer: StreamingInstance,
     onChange: (streams: Array<MediaStream>) => void
 ): void {
-    const streamsRef = useMemo<{ ref: Array<MediaStream> }>(() => ({ ref: Array.from(peer.streams) }), [peer])
     useEffect(() => {
         const subscription = peer.streamsObservable.subscribe({
-            next: (streams: Array<MediaStream>) => {
-                streamsRef.ref = streams
-                onChange(streamsRef.ref)
-            },
+            next: (streams: Array<MediaStream>) => onChange(streams),
         })
         return () => subscription.unsubscribe()
-    }, [peer, onChange, streamsRef])
+    }, [peer, onChange])
 }
 
 export function useIncommingPeerStreams(peer: StreamingInstance): Array<MediaStream> {
-    const [streams, setStreams] = useState<Array<MediaStream>>([])
+    const [streams, setStreams] = useState<Array<MediaStream>>(Array.from(peer.streams))
     useIncommingPeerStreamsChange(peer, setStreams)
     return streams
 }
